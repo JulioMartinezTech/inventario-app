a3f1c7e11cc89eb9bc29d0ae844389f4a5eb2bfe
Julio Martinez <info.julioweb@gmail.com>
2026-01-20 19:14:40 +0100

    add logo and fonts

--- src/views/v-home.vue
+++ src/views/v-home.vue
@@ -1,5 +1,11 @@
 <template>
-  <header class="v-home__header"></header>
+  <header class="v-home__header">
+    <img
+      src="../assets/img/Colombiapan-el-sabor-de-lo-nuestro.webp"
+      alt="logo"
+      class="v-home__logo"
+    />
+  </header>
   <div class="v-home__item-list">
     <CItemCard />
     <CItemCard />
@@ -23,8 +29,13 @@ import CItemCard from '../components/c-item-card.vue'
 .v-home__header {
   width: 100%;
   height: 120px;
-  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin-bottom: 20px;
+}
+.v-home__logo {
+  width: 130px;
 }
 .v-home__item-list {
   display: flex;
